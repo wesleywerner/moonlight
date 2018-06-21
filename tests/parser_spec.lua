92f6ff8f6a6a02efb4ecb4ef602567089db0e62f
Wesley <wesley.werner@gmail.com>
2018-06-21 06:00:48 +0200

    Fix moving the parse function to the api namespace.

--- tests/parser_spec.lua
+++ tests/parser_spec.lua
@@ -2,17 +2,8 @@ describe ("parser", function()
 
 	local ml = require("src/moonlight")
 
-	it ("verb noun", function() 
-		local result = ml:parse("open door")		
-		local expected = {
-			verb="open",
-			nouns={"door"}
-			}
-		assert.are.same(expected, result)
-	end)
-	
-	it ("ignored words", function() 
-		local result = ml:parse("open the door")		
+	it ("verb noun", function()
+		local result = ml.api.parse(ml, "open door")
 		local expected = {
 			verb="open",
 			nouns={"door"}
@@ -20,53 +11,62 @@ describe ("parser", function()
 		assert.are.same(expected, result)
 	end)
 
-	it ("listing nouns: the gargoyle", function() 
-		local result = ml:parse("give sword to gargoyle")		
+	it ("ignored words", function()
+		local result = ml.api.parse(ml, "open the door")
+		local expected = {
+			verb="open",
+			nouns={"door"}
+			}
+		assert.are.same(expected, result)
+	end)
+
+	it ("listing nouns: the gargoyle", function()
+		local result = ml.api.parse(ml, "give sword to gargoyle")
 		local expected = {
 			verb="give",
 			nouns={"sword","gargoyle"}
 			}
 		assert.are.same(expected, result)
 	end)
-	
-	it ("listing nouns: the gate and the skeleton key", function() 
-		local result = ml:parse("unlock the gate with the skeleton key", {"gate", "skeleton key"})		
+
+	it ("listing nouns: the gate and the skeleton key", function()
+		local result = ml.api.parse(ml, "unlock the gate with the skeleton key", {"gate", "skeleton key"})
 		local expected = {
 			verb="unlock",
 			nouns={"gate","skeleton key"}
 			}
 		assert.are.same(expected, result)
 	end)
-	
-	it ("listing nouns: the gate and the skeleton key (lazy)", function() 
-		local result = ml:parse("unlock gate skel", {"gate", "skeleton key"})		
+
+	it ("listing nouns: the gate and the skeleton key (lazy)", function()
+		local result = ml.api.parse(ml, "unlock gate skel", {"gate", "skeleton key"})
 		local expected = {
 			verb="unlock",
 			nouns={"gate","skeleton key"}
 			}
 		assert.are.same(expected, result)
 	end)
-	
-	it ("listing nouns: the unseen tablet", function() 
-		local result = ml:parse("ask the wise guy about an unseen tablet", {"wise guy", "computer"})		
+
+	it ("listing nouns: the unseen tablet", function()
+		local result = ml.api.parse(ml, "ask the wise guy about an unseen tablet", {"wise guy", "computer"})
 		local expected = {
 			verb="ask",
 			nouns={"wise guy", "unseen", "tablet"}
 			}
 		assert.are.same(expected, result)
 	end)
-	
-	it ("interpolate synonymns: look/examine", function() 
-		local result = ml:parse("look at the gate")		
+
+	it ("interpolate synonymns: look/examine", function()
+		local result = ml.api.parse(ml, "look at the gate")
 		local expected = {
 			verb="examine",
 			nouns={"gate"}
 			}
 		assert.are.same(expected, result)
 	end)
-	
-	it ("interpolate synonymns: get/take", function() 
-		local result = ml:parse("get an apple")		
+
+	it ("interpolate synonymns: get/take", function()
+		local result = ml.api.parse(ml, "get an apple")
 		local expected = {
 			verb="take",
 			nouns={"apple"}
@@ -74,8 +74,8 @@ describe ("parser", function()
 		assert.are.same(expected, result)
 	end)
 
-	it ("interpolate synonymns: put/insert", function() 
-		local result = ml:parse("put the box on the table")		
+	it ("interpolate synonymns: put/insert", function()
+		local result = ml.api.parse(ml, "put the box on the table")
 		local expected = {
 			verb="insert",
 			nouns={"box","table"}
@@ -83,8 +83,8 @@ describe ("parser", function()
 		assert.are.same(expected, result)
 	end)
 
-	it ("interpolate synonymns: x/examine", function() 
-		local result = ml:parse("x red apple", {"red apple"})		
+	it ("interpolate synonymns: x/examine", function()
+		local result = ml.api.parse(ml, "x red apple", {"red apple"})
 		local expected = {
 			verb="examine",
 			nouns={"red apple"}
@@ -92,8 +92,8 @@ describe ("parser", function()
 		assert.are.same(expected, result)
 	end)
 
-	it ("interpolate synonymns: i/inventory", function() 
-		local result = ml:parse("i")		
+	it ("interpolate synonymns: i/inventory", function()
+		local result = ml.api.parse(ml, "i")
 		local expected = {
 			verb="inventory",
 			nouns={ }
@@ -101,8 +101,8 @@ describe ("parser", function()
 		assert.are.same(expected, result)
 	end)
 
-	it ("recognizing known nouns: the old man", function() 
-		local result = ml:parse("talk to the old man", {"old man"})		
+	it ("recognizing known nouns: the old man", function()
+		local result = ml.api.parse(ml, "talk to the old man", {"old man"})
 		local expected = {
 			verb="talk",
 			nouns={"old man"}
@@ -110,8 +110,8 @@ describe ("parser", function()
 		assert.are.same(expected, result)
 	end)
 
-	it ("recognizing known nouns: the red stone", function() 
-		local result = ml:parse("take red", {"red stone", "blue stone"})		
+	it ("recognizing known nouns: the red stone", function()
+		local result = ml.api.parse(ml, "take red", {"red stone", "blue stone"})
 		local expected = {
 			verb="take",
 			nouns={"red stone"}
@@ -119,8 +119,8 @@ describe ("parser", function()
 		assert.are.same(expected, result)
 	end)
 
-	it ("recognizing known nouns: the wise guy", function() 
-		local result = ml:parse("ask the wise guy about the computer", {"wise guy", "computer"})		
+	it ("recognizing known nouns: the wise guy", function()
+		local result = ml.api.parse(ml, "ask the wise guy about the computer", {"wise guy", "computer"})
 		local expected = {
 			verb="ask",
 			nouns={"wise guy", "computer"}
@@ -128,8 +128,8 @@ describe ("parser", function()
 		assert.are.same(expected, result)
 	end)
 
-	it ("recognizing known nouns: the wise guy (lazy)", function() 
-		local result = ml:parse("ask guy the comp", {"wise guy", "computer"})		
+	it ("recognizing known nouns: the wise guy (lazy)", function()
+		local result = ml.api.parse(ml, "ask guy the comp", {"wise guy", "computer"})
 		local expected = {
 			verb="ask",
 			nouns={"wise guy", "computer"}
@@ -137,8 +137,8 @@ describe ("parser", function()
 		assert.are.same(expected, result)
 	end)
 
-	it ("recognizing known nouns: the wormwood herb", function() 
-		local result = ml:parse("eat herb", {"wormwood herb"})		
+	it ("recognizing known nouns: the wormwood herb", function()
+		local result = ml.api.parse(ml, "eat herb", {"wormwood herb"})
 		local expected = {
 			verb="eat",
 			nouns={"wormwood herb"}
@@ -146,8 +146,8 @@ describe ("parser", function()
 		assert.are.same(expected, result)
 	end)
 
-	it ("recognizing directions: going northwest", function() 
-		local result = ml:parse("go northwest")		
+	it ("recognizing directions: going northwest", function()
+		local result = ml.api.parse(ml, "go northwest")
 		local expected = {
 			verb="go",
 			direction="northwest",
@@ -156,8 +156,8 @@ describe ("parser", function()
 		assert.are.same(expected, result)
 	end)
 
-	it ("recognizing directions: looking east", function() 
-		local result = ml:parse("look e")		
+	it ("recognizing directions: looking east", function()
+		local result = ml.api.parse(ml, "look e")
 		local expected = {
 			verb="examine",
 			direction="e",
@@ -166,8 +166,8 @@ describe ("parser", function()
 		assert.are.same(expected, result)
 	end)
 
-	it ("recognizing directions: in", function() 
-		local result = ml:parse("look in mirror")		
+	it ("recognizing directions: in", function()
+		local result = ml.api.parse(ml, "look in mirror")
 		local expected = {
 			verb="examine",
 			nouns={"mirror"},
